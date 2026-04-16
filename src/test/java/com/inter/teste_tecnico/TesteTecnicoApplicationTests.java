c6fd851ecd70583a59808b42af1addc502a0653d
Pedro Fortunato Oliveira <pedro.oliveira@logpyx.com>
2026-04-16 01:21:06 -0300

    Spring start

---
+++ src/test/java/com/inter/teste_tecnico/TesteTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inter.teste_tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
